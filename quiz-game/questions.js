d6e495f53a4a864c3b4f234f2923dba41e5ac394
Sameeksh-shetty <52641234+Sameeksh-shetty@users.noreply.github.com>
2026-03-31 10:09:52 +0530

    Create questions.js

---
+++ quiz-game/questions.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    question: "What is 2 + 2?",
+    options: ["3", "4", "5", "6"],
+    answer: 1
+  },
+  {
+    question: "Capital of India?",
+    options: ["Mumbai", "Delhi", "Kolkata", "Chennai"],
+    answer: 1
+  },
+  {
+    question: "Which is a programming language?",
+    options: ["HTML", "CSS", "JavaScript", "Photoshop"],
+    answer: 2
+  },
+  {
+    question: "Sun rises from?",
+    options: ["West", "North", "East", "South"],
+    answer: 2
+  },
+  {
+    question: "5 * 3 = ?",
+    options: ["15", "10", "20", "25"],
+    answer: 0
+  }
+];
